49da5510ca778b0b63bbf5ba338706b4be9c146f
C0routine <whdrjsco@gmail.com>
2024-12-11 22:26:24 +0900

    fix: Constants 분리

---
+++ src/constants.rs
@@ -0,0 +1,7 @@
+pub const YOUTUBE_API_KEY: &str = "";
+
+pub const CHANNEL_NAME: &str = "삼프로TV 3PROTV";
+
+pub const YOUTUBE_API_URL: &str = "https://www.googleapis.com/youtube/v3";
+
+pub const SAMPRO_TV_CHANNEL_ID: &str = "UChlv4GSd7OQl3js-jkLOnFA";
